9a341164f27e22a0e0e0227b422448cf953ca124
Mikael Rizzuto <mikael.r.joaquim@gmail.com>
2025-03-13 21:32:26 -0300

    13/03 noite

--- screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosTemporada(@JsonAlias("Season") Integer numero,
-                             @JsonAlias("Episodes") List<DadosEpisodio> episodio) {
+                             @JsonAlias("Episodes") List<DadosEpisodio> episodios) {
 }
